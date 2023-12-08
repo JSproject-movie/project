8e92d145876c7dfa40b1eddc2d90ecce44db1207
Ivy <ivy7661@gmail.com>
2023-12-08 15:30:34 +0800

    cors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const auth = require("json-server-auth");
@@ -5,6 +6,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
+server.use(cors());
 server.use(middlewares);
 
 //將 JSON-Server 和 Router 的數據同步
