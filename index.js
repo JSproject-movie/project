479aa9e2ee89489ca5f73c605530567c077f3483
Ivy <ivy7661@gmail.com>
2023-12-08 15:25:37 +0800

    fix auth

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
+const auth = require("json-server-auth");
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
