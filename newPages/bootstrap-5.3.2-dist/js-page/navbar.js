11d116b3b1831733551a7aeef8482595453ba7aa
Ivy <ivy7661@gmail.com>
2023-12-10 20:59:26 +0800

    movie db

--- newPages/bootstrap-5.3.2-dist/js-page/navbar.js
+++ newPages/bootstrap-5.3.2-dist/js-page/navbar.js
@@ -1,3 +1,16 @@
 const searchInput = document.querySelector(".search-input");
 // console.log(searchInput);
 let movieIntroData = [];
+
+function getMovieIntroData() {
+  axios
+    .get(`https://testrender-tga5.onrender.com/movieDatas`)
+    .then(function (res) {
+      // console.log(res.data);
+      movieIntroData = res.data;
+    })
+    .catch(function (error) {
+      console.log(error.res);
+    });
+}
+getMovieIntroData();
