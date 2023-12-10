384326539e818abaa8dbb37f99318afa9e9aee1d
Ivy <ivy7661@gmail.com>
2023-12-10 23:04:46 +0800

    search

--- newPages/bootstrap-5.3.2-dist/js-page/navbar.js
+++ newPages/bootstrap-5.3.2-dist/js-page/navbar.js
@@ -31,9 +31,9 @@ function renderSearchResult(movieIntroData) {
       <h2>${item.movieName}(${item.movieEnglishName})</h2>
       <p>語言：${item.language}</p>
       <p>片長：${item.movieLength}</p>
-      <p>導演：安東尼魯索(AnthonyRusso),喬羅素(JoeRusso)</p>
+      <p>導演：${item.personnelInformation.director}</p>
       <p>
-        演員：布萊德利庫柏(BradleyCooper),布麗拉森(BrieLarson),克里斯漢斯沃(ChrisHemsworth),克里斯伊凡(ChrisEvans)...
+        演員：${item.personnelInformation.actor}
       </p>
     </div>
   </div>
@@ -48,10 +48,10 @@ function renderSearchResult(movieIntroData) {
     <span class="ml-30"></span>
     <p>3.8</p>
     <span class="ml-30"></span>
-    <p>(3000人評價)</p>
+    <p>(${item.reviewersNumber}人評價)</p>
   </div>
   <p class="search-card-p">
-    如果地球上最強大的英雄想要阻止頑皮的洛基和他的外星軍隊奴役人類，他們就必須團結起來，學會團隊作戰。
+    ${item.movieIntroduction}
   </p>
   <button class="btn-search-result">
     <a href="#" class="text-btn-search">read more </a>
